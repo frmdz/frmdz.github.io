3aa9afb26fd9ca9fe29f9ef5dee73471cf58fb43
Francisco Mendoza <33354499+frmdz@users.noreply.github.com>
2018-04-13 23:39:19 -0500

    Add files via upload

--- Flappyblock/controles.js
+++ Flappyblock/controles.js
@@ -4,18 +4,22 @@ document.addEventListener("keyup", kup);
 let tap = false;
 
 function kdown(e) {
-  if (e.keyCode == 32 && tap == false && gameover == false ){
-    tap=true;
-    pajaro_1.aletear();
-  }
-  if (e.keyCode == 32 && start == false) {
-    start = true;
-  }
-  if (e.keyCode == 32 && gameover == true) {
-    restart();
+  if (e.keyCode == 32 ) {
+    if (tap == false && gameover == false ){
+      tap=true;
+      pajaro_1.aletear();
+    }
+
+    if (start == false) {
+      start = true;
+    } else if (gameover == true) {
+      restart();
+    }
   }
 }
 
 function kup(e) {
-  tap = false;
+  if (e.keyCode == 32 ) {
+    tap = false;
+  }
 }
